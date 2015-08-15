d3172be8654b1973781470fac81f29840f4a62e0
Ryan Ralph <ryan@ryanralph.net>
2015-08-15 18:09:10 +1000

    Testing python scripts

---
+++ pythontrace.py
@@ -0,0 +1,29 @@
+import json, urllib
+from urllib import urlencode
+import googlemaps
+import poly
+start = "Bridgewater, Sa, Australia"
+finish = "Stirling, SA, Australia"
+
+url = 'http://maps.googleapis.com/maps/api/directions/json?%s' % urlencode((
+            ('origin', start),
+            ('destination', finish)
+ ))
+ur = urllib.urlopen(url)
+result = json.load(ur)
+
+location = []
+for i in range (0, len (result['routes'][0]['legs'][0]['steps'])):
+    j = result['routes'][0]['legs'][0]['steps'][i]['polyline'] 
+    location.append(poly.decode(j['points']))
+    #print poly.decode(j['points'])
+    
+
+    
+    
+
+for polyline in location:
+
+	for coord in polyline:
+		url = """https://maps.googleapis.com/maps/api/streetview?size=400x400&location="%s"&fov=90&heading=235&pitch=10"""%(str(coord[0])+str(coord[1]))
+		print url 
