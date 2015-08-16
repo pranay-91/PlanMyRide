d69be6c23ade401995239eff0b59419b2772c886
Ryan Ralph <ryan@ryanralph.net>
2015-08-16 10:30:32 +1000

    It works!

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, redirect, request, jsonify
 import os
 import json
 import sys
+
 sys.path.append( 'source' )
 import pythontrace
 
@@ -12,11 +13,18 @@ tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 
 app = Flask(__name__, template_folder=tmpl_dir)
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['POST', 'GET'])
+@app.route('/index', methods=['POST', 'GET'])
 
 def home():
-	return render_template('./index.html')
+	if request.method == "GET":
+		locations = []
+		return render_template('./index.html', locations=locations)
+
+	start=request.form['start_loc']
+	end=request.form['end_loc']
+	locations = pythontrace.enter_route(start, end, 'driving')
+	return render_template('./index.html', locations=locations)
 
 
 @app.route('/test')
